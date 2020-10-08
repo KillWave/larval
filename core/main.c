0839ecb3a403fa41d7e3285b35973d88ec59fa8c
KillWave <1353207933@qq.com>
2020-10-08 13:54:19 +0800

    class modify

--- core/main.c
+++ core/main.c
@@ -1,51 +1,45 @@
 #include <stdio.h>
 #include "include/Exception.h"
 #include "include/Class.h"
-
 interface
 {
     int a;
+    string s;
     void (*f)(void);
 }
 Test;
-
 class Persion implements(Test)
 {
-    
     constructor(arguments){
-       
-        
-        printf("%d\n",next(int));
-        printf("%s\n",next(string));
-
+        this->a = next(int);
+        this->s = next(string);
         destroy(arguments);
     }
-    // printf("args = %d\n",*arguments);
-    // va_arg(arguments,int);
-     
-    // printf("%d\n",*arguments);
-    // va_end(arguments);	
-  
-    // this->a = 12;
-    // this->f = lamda(void, (void), {
-    //     printf("class Test %d\n", this->a);
-    // });
 };
 
 int main()
 {
+    
+    autofree Test *t = new(Test,Persion,12,"hello constructor");
+    printf("Persion.s:%s\n",t->s);
+    printf("Persion.a:%d\n",t->a);
+
+
+
+
+
+
+
+
+
+
     fn(void, f, (void), {
         printf("123\n");
     });
     f();
-
     lamda(void, (void), {
         printf("456\n");
     })();
-
-    autofree Test *t = new(Test,Persion,0,"hello constructor");
-    // t->f();
-
     try
     {
         int a = 1, b = 0, c;
